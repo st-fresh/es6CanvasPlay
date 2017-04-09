6f747c1a33afd3e960ef86525ededaca31eb05f5
System <none>
2017-04-09 13:51:24 +0000

    🚒👨‍❤️‍💋‍👨 Updated with Glitch

--- public/organizeMyCoords.js
+++ public/organizeMyCoords.js
@@ -49,12 +49,18 @@ let k; //|||||  This is a global !
   /////// DISPLAY RESULTS (CANVAS PREP & DRAWING) ///////// 
   //
   //{-------------------------------------
-  //i./--> Print ordered data array to conole. 
+  //i./--> Print ordered data array to console ||| see //iii.vi/
   //--> .log(answer) ||| at //iii.v/ commend-out ' delete d[j+m].hyp; ' to add .hyp props back to data.
-    let display = (source) => 
+    let display = (source, clicked) => 
     {
     //
-      let [DATA] = [source.splice(26, 52)]; 
+      let DATA = source.splice(26, 52), f = $('footer'),stringJSON =  JSON.stringify(DATA, null, "  ");
+      f.append("<br /><br />" + stringJSON);
+      
+      if(clicked) {
+        f[0].innerHTML = "";
+        f.append("<br /><br />" + stringJSON);
+      }
     //---> DATA represents the answer to this challenge.. 
     //---> without visual canvas representation, clean(currently cleaning) & BRANCH after you send to display!
     // 
@@ -81,11 +87,11 @@ let k; //|||||  This is a global !
   //       DATA = source.splice(0, 26); //this is answer to challenge without visual canvas representation, clean & BRANCH after you send to display!
   //     }
   ////{-------------------------------------
-  /////// DISPLAY RESULTS (CANVAS PREP & DRAWING) ///////// 
+  /////// CLOSE DISPLAY RESULTS (CANVAS PREP & DRAWING) ///////// 
   /////////////////////////////////////////////////////////
   //
   //
-  ////////////////////////////////
+  ////////////////////////////////////////
   /////// GET & CRUNCH DATA ////////////// 
   //{-------------------------------------
   //iii./ Get data first ||| Ensure it's ready for display. 
@@ -147,7 +153,7 @@ let k; //|||||  This is a global !
          //
   //{-------------------------------------
   //iii.vi/--> Compare then Add to sourced coordinates.json in-order: [{least(closest)},..,{greatest(farthest)}] 
-    function compare(d, dist) //--> d = sourced array, dist = distances or hypotenuses or magnitudes
+    function compare(d, dist, click) //--> d = sourced array, dist = distances or hypotenuses or magnitudes
     {
       let [i, j, l, m] = [0, 26, 0, ];
     //--> .inArray ||| display() ||| delete
@@ -160,7 +166,7 @@ let k; //|||||  This is a global !
         delete d[j+m].hyp; 
       //
         if (l === 26) {
-          display(d); //for answer in console and rendered on page
+          display(d, click); //for answer in console and rendered on page
             // prepCanvas(d); //un-comment this line to show answer drawn to canvas
         } //--> Close if in the for
       }//--> Close for
@@ -168,91 +174,80 @@ let k; //|||||  This is a global !
     }//--> Close compare()
   //}-------------------------------------
 
-    //{-------------------------------------
-    //iii.vii/--> Prep and then Send data to compare() at ////iii.v/
-    //--> send() | Array.from() | .length | Math.abs() | Math.sqrt() | .push | .sort | .apply | compare | W.send
-     send = (d, xvals, yvals, click, ...vs) =>
-     {
-     // 
-       d     = dREF;
-       xvals = xvalsREF;
-       yvals = yvalsREF;
-     // 
-       let vx, vy, arb = Array.from('12345678901234567890123456');
-     //--> 1) Handles randomized on-page-load
-       if(!click) {  
-         vx = vs[0][0];
-         vy = vs[0][1];
-       }
-     //--> 2) Handles button clicks
-       if (click) {
-         vx = vs[0][0];
-         vy = vs[0][1];
-       }  
-     //--> send() continues
-     ///--> TESTING --> all tests below should pass before moving forward to compare() and use canvas.
-     // console.log(vx, vy); //-->  TEST --> Check your random and hard-coded inputs.
-     // console.log(d); //-->  TEST --> Check your original sourced data-array coordinates.json
-     // console.log(xvals); //-->  TEST --> Check x-values split from sourced data-array, see //iii.iv 
-     // console.log(yvals); //-->  TEST --> Check y-values split from sourced data-array, see //iii.iv 
-     //   
-       let [fvx, fvy, hyps, i] = [ , , [], 0];
-     //
-       for( i ; i < xvals.length ; i++ ) 
-       {
-       //--> Math.abs() Accounts here for (-) values ||| Finds positive magnitudes ||| see *summary.md*
-         fvx      = Math.abs( (xvals[i] - vx)*(xvals[i] - vx) );
-         fvy      = Math.abs( (yvals[i] - vy)*(yvals[i] - vy) );
-       //
-         d[i].hyp = Math.sqrt(fvx + fvy); //--> See *summary.md* file for why Math.floor() not used here.
-       //
-         hyps.push(d[i].hyp); 
-       //
-         if (hyps.length === 26) 
-         {
+  //{-------------------------------------
+  //iii.vii/--> Prep and then Send data to compare() at ////iii.v/
+  //--> send() | Array.from() | .length | Math.abs() | Math.sqrt() | .push | .sort | .apply | compare | W.send
+    send = (d, xvals, yvals, click, ...vs) =>
+    {
+    // 
+      d     = dREF;
+      xvals = xvalsREF;
+      yvals = yvalsREF;
+    // 
+      let vx, vy, arb = Array.from('12345678901234567890123456');
+    //--> 1) Handles randomized on-page-load
+      if(!click) {  
+        vx = vs[0][0];
+        vy = vs[0][1];
+      }
+    //--> 2) Handles button clicks
+      if (click) {
+        vx = vs[0][0];
+        vy = vs[0][1];
+      }  
+    //--> send() continues
+    ///--> TESTING --> all tests below should pass before moving forward to compare() and use canvas.
+    // console.log(vx, vy); //-->  TEST --> Check your random and hard-coded inputs.
+    // console.log(d); //-->  TEST --> Check your original sourced data-array coordinates.json
+    // console.log(xvals); //-->  TEST --> Check x-values split from sourced data-array, see //iii.iv 
+    // console.log(yvals); //-->  TEST --> Check y-values split from sourced data-array, see //iii.iv 
+    //   
+      let [fvx, fvy, hyps, i] = [ , , [], 0];
+    //
+      for( i ; i < xvals.length ; i++ ) 
+      { 
+      //--> Math.abs() Accounts here for (-) values ||| Finds positive magnitudes ||| see *summary.md*
+        fvx      = Math.abs( (xvals[i] - vx)*(xvals[i] - vx) );
+        fvy      = Math.abs( (yvals[i] - vy)*(yvals[i] - vy) );
+        d[i].hyp = Math.sqrt(fvx + fvy); //--> See *summary.md* file for why Math.floor() not used here.
+        hyps.push(d[i].hyp); 
+      //
+        if (hyps.length === 26) 
+        {
                   //{-------------------------------------
                   //iii.viii/--> Sort hyps array filled with distances/magnitudes/hypotenuses' least to greatest. 
-           hyps.sort(function(a, b)
+          hyps.sort(function(a, b)
                     { 
                       return a - b;
                     } //--> Close anonymous function() param in sort()
                   //}-------------------------------------
-                   );
-           arb.push.apply(d,arb); //--> Add to sourced array arbitrary values that will hold ordered data.
-         //
-          compare(d, hyps);
-         //
-         } //--> Close if-statement in the for-loop in the send()
-       }//--> Close for-loop in the send() 
-     }//--> Close send() 
-
-     W.send = send;
-    
-   })(window, jQuery); //Close //iii./
-///  
+                 );
+          arb.push.apply(d,arb); //--> Add to sourced array arbitrary values that will hold ordered data.
+          compare(d, hyps, click);
+        } //--> Close if-statement in the for-loop in the send()
+      }//--> Close for-loop in the send() 
+    }//--> Close send() 
+  //--> pass send() to next scope using window.
+    W.send = send;
+  })(window, jQuery); //--> Close //iii./
+/// 
+//}-------------------------------------
+////////////////////////////////
+/////// CLOSE GET & CRUNCH DATA ////////////// 
+ 
 ///////////////////////////  
 //// DYNAMIC-LISTENER ////
   click = (x,y, inputs, clicker) => 
     { 
-      //->>//SHOWER!!
       clicker = true; 
       let button; //console.log("test");
-      //--inputs->>
       if($(inputs)){ 
-        //--->
         button = $('#button');
-        //
-        console.log(button);
-        // console.log(x.val(), y.val());
-        //--->
       }
 
       if(clicker) { 
-        //--->
         return W.send(0,0,0,clicker,[x.val(),y.val()]);
-        //--->func
       }
-      //
     }
 ///// DYNAMIC-LISTENER /////
 ///////////////////////////  
